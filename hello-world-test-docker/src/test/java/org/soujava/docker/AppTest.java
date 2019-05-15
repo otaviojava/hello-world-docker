a37a49c3bc3615ee61707b2a182f84cbcec4859f
Otavio Santana <otaviopolianasantana@gmail.com>
2019-05-14 22:23:44 -0300

    adds new test-container module

---
+++ hello-world-test-docker/src/test/java/org/soujava/docker/AppTest.java
@@ -0,0 +1,13 @@
+package org.soujava.docker;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
