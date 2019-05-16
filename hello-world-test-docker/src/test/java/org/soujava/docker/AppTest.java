65d38c75f90a0e4a213cb592d3c9194e86880829
Otavio Santana <otaviopolianasantana@gmail.com>
2019-05-16 16:12:23 -0300

    updates the version test container

--- hello-world-test-docker/src/test/java/org/soujava/docker/AppTest.java
+++ hello-world-test-docker/src/test/java/org/soujava/docker/AppTest.java
@@ -1,13 +1,22 @@
 package org.soujava.docker;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        final GenericContainer mongodb =
+                new GenericContainer("mongo:latest")
+                        .withExposedPorts(27017)
+                        .waitingFor(Wait.defaultWaitStrategy());
+
+        mongodb.start();
+        Assertions.assertNotNull(mongodb.getContainerIpAddress());
+        Assertions.assertNotNull(mongodb.getFirstMappedPort());
+        mongodb.stop();
     }
 }
